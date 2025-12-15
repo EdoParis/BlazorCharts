40f1f60787f5d97ba924b43fdae3c902d035ef58
Edoardo Paris <edoardo.paris@gmail.com>
2025-12-15 18:40:58 +0100

    CHARTS: completed histogram

--- BlazorCharts/Structures/Axis.cs
+++ BlazorCharts/Structures/Axis.cs
@@ -39,14 +39,36 @@
                     IsDefault = false
                 };
             }
+        }
 
-            /*Step = (int)((Max - Min) / 5);
+        public IEnumerable<double> Ticks()
+        {
+            List<double> ticks = new List<double>();
 
-            if (Step > 5)
-                Step = Step - Step % 5;
+            if (Max != Min)
+            {
+                int step = (int)((Max - Min) / 5);
 
-            if (Step <= 0)
-                Step = 1;*/
+                if (step > 5)
+                    step = step - step % 5;
+
+                if (step <= 0)
+                    step = 1;
+
+                for (double t = Min; t < Max; t += step)
+                {
+                    for (int i = 1; i < 6; i++)
+                    {
+                        double tick = t + i * step / 5d;
+
+                        if (tick <= Max)
+                            ticks.Add(t + i * step / 5d);
+                        else
+                            break;
+                    }
+                }
+            }
+            return ticks;
         }
     }
 }

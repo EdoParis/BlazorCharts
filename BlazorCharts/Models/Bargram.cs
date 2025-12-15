7eb64ccb8f4fd26ddc80cd231faf44aafebc4594
Edoardo Paris <edoardo.paris@gmail.com>
2025-12-15 18:41:25 +0100

    CHARTS: added barchart

---
+++ BlazorCharts/Models/Bargram.cs
@@ -0,0 +1,57 @@
+﻿using BlazorCharts.Structures;
+using System.Collections;
+using System;
+
+namespace BlazorCharts.Models
+{
+    public class Bargram : IEnumerable<KeyValuePair<String, Bin>>
+    {
+        private List<KeyValuePair<String, Bin>> bars;
+        public Axis AxisX { get; private set; }
+        public Axis AxisY { get; private set; }
+        public string TitleX { get; private set; }
+        public string TitleY { get; private set; }
+        public int BarsCount { get => bars?.Count ?? default; }
+
+        public Bargram(string title_x, string title_y)
+        {
+            bars = new List<KeyValuePair<String, Bin>>();
+            TitleX = title_x;
+            TitleY = title_y;
+            AxisX = new Axis();
+            AxisY = new Axis();
+        }
+
+        public void Clear()
+        {
+            bars.Clear();
+            AxisX = new Axis();
+            AxisY = new Axis();
+        }
+
+        public void Add(Bar bar)
+        {
+            Bin bin = new Bin()
+            {
+                Min = 2 * bars.Count,
+                Max = 2 * bars.Count + 1,
+                Value = bar.Value
+            };
+            bars.Add(KeyValuePair.Create(bar.Label, bin));
+            AxisX = AxisX.Update(bin.Min);
+            AxisX = AxisX.Update(bin.Max);
+            AxisY = AxisY.Update(default);
+            AxisY = AxisY.Update(bin.Value);
+        }
+
+        public IEnumerator<KeyValuePair<String, Bin>> GetEnumerator()
+        {
+            return bars.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
--- Demo/Demo.Client/Pages/Home.razor.cs
+++ Demo/Demo.Client/Pages/Home.razor.cs
@@ -6,18 +6,26 @@ namespace DemoApp.Pages
     public partial class Home
     {
         private Histogram histogram;
+        private Bargram bargram;
 
         protected override void OnInitialized()
         {
             histogram = new Histogram("asseX", "asseY");
+            bargram = new Bargram("asse-1", "asse-2");
 
-            for (int i=0; i<20; i++)
+            for (int i=0; i<22; i++)
             {
                 histogram.Add(new Bin()
                 {
                     Min = i,
                     Max = i + 1,
                     Value = i * i
+                });
+
+                bargram.Add(new Bar()
+                {
+                    Label = $"{i + 1}",
+                    Value = 10 + i
                 });
             }
         }

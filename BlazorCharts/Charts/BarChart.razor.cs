a2566b2bc141c47196b7ba4ade99728238b7100f
Edoardo Paris <edoardo.paris@gmail.com>
2026-06-08 19:22:42 +0200

    CHARTS: added horizontal barchart component and removed direction param from barchart

--- BlazorCharts/Charts/BarChart.razor.cs
+++ BlazorCharts/Charts/BarChart.razor.cs
@@ -10,7 +10,7 @@ namespace BlazorGraphs.Charts
         private const int VIEW = 1000;
         private const int PADDING = 100;
 
-        [Parameter] public Positioning Direction { get; set; }
+        [Parameter] [Obsolete("use HorizontalBarChart component instead")] public Positioning Direction { get; set; }
         [Parameter] public Bargram Model { get; set; }
         [Parameter] public EventCallback<Bar> OnClick {get; set; }
         private int width = VIEW;
@@ -18,26 +18,16 @@ namespace BlazorGraphs.Charts
         private int padding = PADDING;
         private int offsetH => padding;
         private int offsetV => height - padding;
-        private double scaleH => (width - 2 * padding) / (Direction == Positioning.Vertical ? Model.BinAxis.Size : Model.ValAxis.Size);
-        private double scaleV => (height - 2 * padding) / (Direction == Positioning.Vertical ? Model.ValAxis.Size : Model.BinAxis.Size);
+        private double scaleH => (width - 2 * padding) / Model.BinAxis.Size;
+        private double scaleV => (height - 2 * padding) / Model.ValAxis.Size;
 
-        private string VerticalBinPath(Bin bin)
+        private string BinPath(Bin bin)
         {
             return $"M {offsetH + (int)((bin.Min - Model.BinAxis.Min) * scaleH)} {offsetV}" +
                    $"v {-(int)((bin.Value - Model.ValAxis.Min) * scaleV)} " +
                    $"h {(int)((bin.Max - bin.Min) * scaleH)} " +
                    $"v {(int)((bin.Value - Model.ValAxis.Min) * scaleV)} " +
                    $"h {(int)((bin.Min - bin.Max) * scaleH)} " +
-                   $"Z";
-        }
-
-        private string HorizontalBinPath(Bin bin)
-        {
-            return $"M {offsetH} {padding + (int)((bin.Min - Model.BinAxis.Min) * scaleV)}" +
-                   $"h {(int)((bin.Value - Model.ValAxis.Min) * scaleH)} " +
-                   $"v {(int)((bin.Max - bin.Min) * scaleV)} " +
-                   $"h {-(int)((bin.Value - Model.ValAxis.Min) * scaleH)} " +
-                   $"v {(int)((bin.Min - bin.Max) * scaleV)} " +
                    $"Z";
         }
     }
---
+++ BlazorCharts/Charts/HorizontalBarChart.razor.cs
@@ -0,0 +1,32 @@
+﻿using BlazorGraphs.Models;
+using BlazorGraphs.Structures;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorGraphs.Charts
+{
+    public partial class HorizontalBarChart
+    {
+        private const int VIEW = 1000;
+        private const int PADDING = 100;
+
+        [Parameter] public Bargram Model { get; set; }
+        [Parameter] public EventCallback<Bar> OnClick {get; set; }
+        private int width = VIEW;
+        private int height = VIEW;
+        private int padding = PADDING;
+        private int offsetH => padding;
+        private int offsetV => height - padding;
+        private double scaleH => (width - 2 * padding) / Model.ValAxis.Size;
+        private double scaleV => (height - 2 * padding) / Model.BinAxis.Size;
+
+        private string BinPath(Bin bin)
+        {
+            return $"M {offsetH} {padding + (int)((bin.Min - Model.BinAxis.Min) * scaleV)}" +
+                   $"h {(int)((bin.Value - Model.ValAxis.Min) * scaleH)} " +
+                   $"v {(int)((bin.Max - bin.Min) * scaleV)} " +
+                   $"h {-(int)((bin.Value - Model.ValAxis.Min) * scaleH)} " +
+                   $"v {(int)((bin.Min - bin.Max) * scaleV)} " +
+                   $"Z";
+        }
+    }
+}

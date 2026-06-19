4f974b27c6bd35d5404bd5ba8225b3858a0960d6
Edoardo Paris <edoardo.paris@gmail.com>
2026-06-19 20:29:30 +0200

    STRUCTURES: added default themes

--- BlazorCharts/Structures/Theme.cs
+++ BlazorCharts/Structures/Theme.cs
@@ -14,9 +14,9 @@ namespace BlazorGraphs.Structures
         {
             get => new Theme()
             {
-                BackgroundColor = Color.FromArgb(10, 10, 10),
-                TextColor = Color.FromArgb(245, 245, 245),
-                AxisColor = Color.FromArgb(245, 245, 245)
+                BackgroundColor = Color.FromArgb(18, 18, 20),
+                TextColor = Color.FromArgb(248, 249, 250),
+                AxisColor = Color.FromArgb(248, 249, 250)
             };
         }
 
@@ -24,9 +24,39 @@ namespace BlazorGraphs.Structures
         {
             get => new Theme()
             {
-                BackgroundColor = Color.FromArgb(245, 245, 245),
-                TextColor = Color.FromArgb(10, 10, 10),
-                AxisColor = Color.FromArgb(10, 10, 10)
+                BackgroundColor = Color.FromArgb(248, 249, 250),
+                TextColor = Color.FromArgb(33, 37, 41),
+                AxisColor = Color.FromArgb(33, 37, 41)
+            };
+        }
+
+        public static Theme Arctic
+        {
+            get => new Theme()
+            {
+                BackgroundColor = Color.FromArgb(240, 244, 248),
+                TextColor = Color.FromArgb(71, 85, 105),
+                AxisColor = Color.FromArgb(148, 163, 184)
+            };
+        }
+
+        public static Theme Beach
+        {
+            get => new Theme()
+            {
+                BackgroundColor = Color.FromArgb(243, 239, 224),
+                TextColor = Color.FromArgb(42, 68, 94),
+                AxisColor = Color.FromArgb(190, 190, 160)
+            };
+        }
+
+        public static Theme Neon
+        {
+            get => new Theme()
+            {
+                BackgroundColor = Color.FromArgb(10, 10, 20),
+                TextColor = Color.FromArgb(0, 240, 255),
+                AxisColor = Color.FromArgb(0, 200, 230)
             };
         }
     }

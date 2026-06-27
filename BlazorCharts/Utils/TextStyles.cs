5a5e47295205fd1a2159c7d66a26b93b4669555b
Edoardo Paris <edoardo.paris@gmail.com>
2026-06-27 12:50:31 +0200

    STATIC: added centralized text styles

---
+++ BlazorCharts/Utils/TextStyles.cs
@@ -0,0 +1,57 @@
+﻿namespace BlazorGraphs.Static
+{
+    internal static class TextStyles
+    {
+        private static string Default()
+        {
+            return "font-size: 2.5rem;" +
+                "pointer-events: none;";
+        }
+
+        public static string Start()
+        {
+            return Default() +
+                "text-anchor: start;" +
+                "transform: translateX(0.5em);";
+        }
+
+        public static string Middle()
+        {
+            return Default() +
+                "text-anchor: middle;";
+        }
+
+        public static string End()
+        {
+            return Default() +
+                "text-anchor: end;" +
+                "transform: translateX(-0.5em);";
+        }
+
+        public static string Rotated45()
+        {
+            return Default() + 
+                "transform: rotate(-45deg);" +
+                "transform-box: fill-box;" +
+                "transform-origin: center;";
+        }
+
+        public static string Rotated90()
+        {
+            return Default() +
+                "text-anchor: end;" +
+                "transform: rotate(-90deg) translateX(-0.5em);" +
+                "transform-box: fill-box;" +
+                "transform-origin: right;";
+        }
+
+        public static string Rotated270()
+        {
+            return Default() + 
+                "text-anchor: start;" +
+                "transform: rotate(270deg) translateX(0.5em);" +
+                "transform-box: fill-box;" +
+                "transform-origin: left;";
+        }
+    }
+}

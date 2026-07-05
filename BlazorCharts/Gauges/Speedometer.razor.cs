f462e2a7ddef93f4857a3189e0a6d5afc4ffcbfd
Edoardo Paris <edoardo.paris@gmail.com>
2026-07-05 15:44:31 +0200

    GAUGES: added speedometer

--- BlazorCharts/Gauges/Speedometer.razor.cs
+++ BlazorCharts/Gauges/Speedometer.razor.cs
@@ -5,7 +5,7 @@ using System.Drawing;
 
 namespace BlazorGraphs.Gauges
 {
-    public partial class SemiCircleGauge
+    public partial class Speedometer
     {
         private const int VIEW = 1000;
         private const int PADDING = 100;
@@ -13,45 +13,45 @@ namespace BlazorGraphs.Gauges
         [Parameter] public Theme Theme { get; set; }
         [Parameter] public Gaugegram Model { get; set; }
         private int width = VIEW;
-        private int height = VIEW / 2 + PADDING;
-        private int radius = VIEW / 2 - 2 * PADDING;
+        private int height = VIEW;
+        private int radius = VIEW / 6;
         private int padding = PADDING;
 
         private string ArcPath(double radius, double start, double end)
         {
             int startpoint_x = (int)Math.Round(width / 2 - radius * Math.Cos(start));
-            int startpoint_y = (int)Math.Round(height - padding - radius * Math.Sin(start));
+            int startpoint_y = (int)Math.Round(height / 2 - radius * Math.Sin(start));
             int endpoint_x = (int)Math.Round(width / 2 - radius * Math.Cos(end));
-            int endpoint_y = (int)Math.Round(height - padding - radius * Math.Sin(end));
+            int endpoint_y = (int)Math.Round(height / 2 - radius * Math.Sin(end));
 
             return $"M {startpoint_x} {startpoint_y} " +
-                   $"A {radius} {radius} 0 0 1 {endpoint_x} {endpoint_y}";
+                   $"A {radius} {radius} 0 {(end - start > Math.PI ? 1 : 0)} 1 {endpoint_x} {endpoint_y}";
         }
 
         private string BarPath(double degree)
         {
             double bar_width = padding / 5;
-            double bar_height = 6 * padding / 5;
+            double bar_height = 2.2 * radius;
 
             Point P1 = new Point()
             {
                 X = (int)(width / 2 - (radius + bar_height / 2) * Math.Cos(degree) - bar_width / 2 * Math.Sin(degree)),
-                Y = (int)(height - padding - (radius + bar_height / 2) * Math.Sin(degree) + bar_width / 2 * Math.Cos(degree))
+                Y = (int)(height / 2 - (radius + bar_height / 2) * Math.Sin(degree) + bar_width / 2 * Math.Cos(degree))
             };
             Point P2 = new Point()
             {
                 X = (int)(width / 2 - (radius + bar_height / 2) * Math.Cos(degree) + bar_width / 2 * Math.Sin(degree)),
-                Y = (int)(height - padding - (radius + bar_height / 2) * Math.Sin(degree) - bar_width / 2 * Math.Cos(degree))
+                Y = (int)(height / 2 - (radius + bar_height / 2) * Math.Sin(degree) - bar_width / 2 * Math.Cos(degree))
             };
             Point P3 = new Point()
             {
                 X = (int)(width / 2 - (radius - bar_height / 2) * Math.Cos(degree) + bar_width / 2 * Math.Sin(degree)),
-                Y = (int)(height - padding - (radius - bar_height / 2) * Math.Sin(degree) - bar_width / 2 * Math.Cos(degree))
+                Y = (int)(height / 2 - (radius - bar_height / 2) * Math.Sin(degree) - bar_width / 2 * Math.Cos(degree))
             };
             Point P4 = new Point()
             {
                 X = (int)(width / 2 - (radius - bar_height / 2) * Math.Cos(degree) - bar_width / 2 * Math.Sin(degree)),
-                Y = (int)(height - padding - (radius - bar_height / 2) * Math.Sin(degree) + bar_width / 2 * Math.Cos(degree))
+                Y = (int)(height / 2 - (radius - bar_height / 2) * Math.Sin(degree) + bar_width / 2 * Math.Cos(degree))
             };
 
             return $"M {P1.X} {P1.Y}" +

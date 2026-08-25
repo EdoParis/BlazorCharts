a339958f649699372ff3a2b0dfe088f13cceeb68
Edoardo Paris <edoardo.paris@gmail.com>
2026-08-25 12:12:00 +0200

    SITE: added gauge pages

---
+++ WebSite/WebSite.Client/Pages/PageSpeedometer.razor.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Drawing;
+using BlazorGraphs;
+
+namespace WebApp.Pages
+{
+    public partial class PageSpeedometer : ComponentBase
+    {
+        Gaugegram model1;
+        Gaugegram model2;
+
+        protected override void OnInitialized()
+        {
+            model1 = new Gaugegram(700, 1000, "G1", Color.RoyalBlue);
+            model2 = new Gaugegram(0, 500, "G2", Color.Navy);
+            model1.Value = 800;
+            model2.Value = 170;
+
+            model2.AddBreakpoint(new Breakpoint()
+            {
+                Value = 150,
+                Color = Color.Green,
+                Label = "LV-1"
+            });
+            model2.AddBreakpoint(new Breakpoint()
+            {
+                Value = 250,
+                Color = Color.Gold,
+                Label = "LV-2"
+            });
+            model2.AddBreakpoint(new Breakpoint()
+            {
+                Value = 500,
+                Color = Color.Red,
+                Label = "LV-3"
+            });
+        }
+    }
+}
---
+++ WebSite/WebSite.Client/Pages/PageVerticalGauge.razor.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Drawing;
+using BlazorGraphs;
+
+namespace WebApp.Pages
+{
+    public partial class PageVerticalGauge : ComponentBase
+    {
+        Gaugegram model1;
+        Gaugegram model2;
+        Gaugegram model3;
+
+        protected override void OnInitialized()
+        {
+            model1 = new Gaugegram(700, 1000, "G1", Color.RoyalBlue);
+            model2 = new Gaugegram(500, 1000, "G2", Color.LimeGreen);
+            model3 = new Gaugegram(0, 500, "G3", Color.Navy);
+            model1.Value = 800;
+            model2.Value = 800;
+            model3.Value = 170;
+
+            model3.AddBreakpoint(new Breakpoint()
+            {
+                Value = 150,
+                Color = Color.Green,
+                Label = "LV-1" 
+            });
+            model3.AddBreakpoint(new Breakpoint()
+            {
+                Value = 250,
+                Color = Color.Gold,
+                Label = "LV-2"
+            });
+            model3.AddBreakpoint(new Breakpoint()
+            {
+                Value = 500,
+                Color = Color.Red,
+                Label = "LV-3"
+            });
+        }
+    }
+}

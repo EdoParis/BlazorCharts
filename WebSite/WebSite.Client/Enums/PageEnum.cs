df28b8ad1a10c0a0861615db677caa51942822b1
Edoardo Paris <edoardo.paris@gmail.com>
2026-08-27 12:54:33 +0200

    SITE: improved navigation

---
+++ WebSite/WebSite.Client/Enums/PageEnum.cs
@@ -0,0 +1,26 @@
+﻿namespace WebApp.Enums
+{
+    public enum PageEnum
+    {
+        Home,
+        LineChart,
+        StepChart,
+        ScatterChart,
+        BubbleChart,
+        PieChart,
+        DonutChart,
+        PolarAreaChart,
+        RadarChart,
+        Histogram,
+        VerticalBarChart,
+        HorizontalBarChart,
+        HorizontalGauge,
+        VerticalGauge,
+        SemicircleGauge,
+        Speedometer,
+        LegendBar,
+        Themes,
+        Roadmap,
+        Changelog
+    }
+}
--- WebSite/WebSite.Client/Layout/MainLayout.razor.cs
+++ WebSite/WebSite.Client/Layout/MainLayout.razor.cs
@@ -1,24 +1,40 @@
 ﻿using Microsoft.AspNetCore.Components;
+using WebApp.Enums;
 
 namespace WebApp.Layout
 {
     public partial class MainLayout : LayoutComponentBase
     {
-        private bool HiddenMenu = true;
+        [Inject] public NavigationManager NavManager { get; set; }
+        private PageEnum CurrentPage = PageEnum.Home;
+        private Boolean HiddenMenu = true;
+
+        protected override void OnInitialized()
+        {
+            string current_url = NavManager?.ToBaseRelativePath(NavManager.Uri);
+
+            if (string.IsNullOrEmpty(current_url))
+                return;
+
+            foreach (PageEnum page in Enum.GetValues<PageEnum>())
+            {
+                if (current_url.EndsWith(page.ToString(), StringComparison.OrdinalIgnoreCase))
+                {
+                    CurrentPage = page;
+                    return;
+                }
+            }
+        }
 
         private void OnToggleMenu()
         {
             HiddenMenu = !HiddenMenu;
         }
 
-        private void OnHideMenu()
+        private void OnPage(PageEnum page)
         {
             HiddenMenu = true;
-        }
-
-        private void OnShowMenu()
-        {
-            HiddenMenu = false;
+            CurrentPage = page;
         }
     }
 }

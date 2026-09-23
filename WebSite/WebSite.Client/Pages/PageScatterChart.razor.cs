a0d54b1b8464b04d2881179c9d4b0a1ddbfb7326
Edoardo Paris <edoardo.paris@gmail.com>
2026-09-23 10:23:46 +0200

    SITE: updated scatterchart page with aspect-ratio parameter

--- WebSite/WebSite.Client/Pages/PageScatterChart.razor.cs
+++ WebSite/WebSite.Client/Pages/PageScatterChart.razor.cs
@@ -9,10 +9,12 @@ namespace WebApp.Pages
         private Cartesiangram model;
         private Random random;
         private Color line_color;
+        private Double aspectratio;
 
         protected override void OnInitialized()
         {
             random = new Random();
+            aspectratio = AspectRatio.Square;
             line_color = Color.MediumOrchid;
             model = new Cartesiangram("AxisX", "AxisY");
             List<Datapoint> points1 = new();

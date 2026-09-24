a4a41654b2a0b6d65873605671f50bec94e7b86e
Edoardo Paris <edoardo.paris@gmail.com>
2026-09-24 19:06:58 +0200

    SITE: added new aspectratios

--- BlazorCharts/Constants/AspectRatios.cs
+++ BlazorCharts/Constants/AspectRatios.cs
@@ -3,6 +3,7 @@
     public static class AspectRatios
     {
         public const double Square = 1.0;
+        public const double Higher = 1.0 / 3.0;
         public const double Vertical = 2.0 / 3.0;
         public const double Portrait = 3.0 / 4.0;
         public const double Standard = 4.0 / 3.0;
@@ -10,6 +11,7 @@
         public const double Widescreen = 16.0 / 9.0;
         public const double Panoramic = 2.0;
         public const double Ultrawide = 21.0 / 9.0;
+        public const double Ultralarge = 3.0;
         public const double Maximum = 5.0;
         public const double Minimum = 0.2;
     }

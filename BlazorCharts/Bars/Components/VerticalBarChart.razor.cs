89db47eb57ad0b92208bf9f47609f11e343571c2
Edoardo Paris <edoardo.paris@gmail.com>
2026-09-24 18:14:34 +0200

    CHARTS: added aspect-ratio parameter to verticalbar chart

--- BlazorCharts/Bars/Components/VerticalBarChart.razor.cs
+++ BlazorCharts/Bars/Components/VerticalBarChart.razor.cs
@@ -5,21 +5,11 @@ namespace BlazorGraphs.Components
 {
     public partial class VerticalBarChart
     {
-        private const int VIEW = 1000;
-        private const int PADDING = 100;
-
         [Parameter] public Theme Theme { get; set; }
+        [Parameter] public Double AspectRatio { get; set; } = AspectRatios.Square;
         [Parameter] public Bargram Model { get; set; }
         [Parameter] public EventCallback<Bar> OnClick {get; set; }
-        private int width = VIEW;
-        private int height = VIEW;
-        private int padding = PADDING;
-        private int offsetH => padding;
-        private int offsetV => height - padding;
-        private int originH => Model is null ? offsetH : offsetH - (int)(Model.BinAxis.Min * scaleH);
-        private int originV => Model is null ? offsetV : offsetV + (int)(Model.ValAxis.Min * scaleV);
-        private double scaleH => (width - 2 * padding) / Model.BinAxis.Size;
-        private double scaleV => (height - 2 * padding) / Model.ValAxis.Size;
+        private ViewLayout LayoutView;
         private AxisLayout LayoutAxisY;
         private AxisLayout LayoutAxisX;
         private TextLayout LayoutTitle;
@@ -28,8 +18,9 @@ namespace BlazorGraphs.Components
 
         protected override void OnParametersSet()
         {
-            LayoutAxisX.WithTheme(Theme);
-            LayoutAxisY.WithTheme(Theme);
+            LayoutView.WithAspectRatio(AspectRatio);
+            LayoutAxisX.WithTheme(Theme).From(LayoutView.Padding).To(LayoutView.Width - LayoutView.Padding);
+            LayoutAxisY.WithTheme(Theme).From(LayoutView.Height - LayoutView.Padding).To(LayoutView.Padding).At(LayoutView.Padding);
             LayoutTitle.WithTheme(Theme);
             LayoutLabelsTop.WithTheme(Theme);
             LayoutLabelsBottom.WithTheme(Theme);
@@ -37,25 +28,26 @@ namespace BlazorGraphs.Components
 
         protected override void OnInitialized()
         {
+            LayoutView = ViewLayout.Default();
             LayoutAxisY = AxisLayout.VerticalLayout()
                                     .TicksInternal()
                                     .WithTickSize(20)
                                     .WithTheme(Theme)
-                                    .From(height - padding)
-                                    .To(padding)
-                                    .At(padding);
+                                    .From(LayoutView.Height - LayoutView.Padding)
+                                    .To(LayoutView.Padding)
+                                    .At(LayoutView.Padding);
 
             LayoutAxisX = AxisLayout.HorizontalLayout()
                                     .WithTheme(Theme)
-                                    .From(padding)
-                                    .To(width - padding);
+                                    .From(LayoutView.Padding)
+                                    .To(LayoutView.Width - LayoutView.Padding);
 
             LayoutTitle = TextLayout.StartLayout()
                                     .Medium()
                                     .WithTheme(Theme);
 
-            LayoutLabelsTop = TextLayout.VerticalTopLayout().Medium();
-            LayoutLabelsBottom = TextLayout.VerticalBottomLayout().Medium();
+            LayoutLabelsTop = TextLayout.VerticalTopLayout().Medium().WithTheme(Theme);
+            LayoutLabelsBottom = TextLayout.VerticalBottomLayout().Medium().WithTheme(Theme);
         }
     }
 }
